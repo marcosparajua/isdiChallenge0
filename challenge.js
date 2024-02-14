0fcdadeef2ca4eb9319dc72bc8392d290c645906
marcos parajua <marcosparajua@gmail.com>
2024-02-14 18:25:48 +0100

    challenge0

---
+++ challenge.js
@@ -0,0 +1,8 @@
+const strictEquals = (a, b) => {
+  if (Number.isNaN(a) && Number.isNaN(b)) return false;
+  if (Object.is(a, 0) && Object.is(b, -0)) return true;
+  if (Object.is(a, -0) && Object.is(b, 0)) return true;
+  return Object.is(a, b);
+};
+
+console.log(strictEquals('toalla', 'toalla'));
