4c30ea7b1b566137bf2e41895c6114f115239e32
marcos parajua <marcosparajua@gmail.com>
2024-02-14 19:08:51 +0100

    added module

--- challenge.js
+++ challenge.js
@@ -1,8 +1,6 @@
-const strictEquals = (a, b) => {
+export function strictEquals(a, b) {
   if (Number.isNaN(a) && Number.isNaN(b)) return false;
   if (Object.is(a, 0) && Object.is(b, -0)) return true;
   if (Object.is(a, -0) && Object.is(b, 0)) return true;
   return Object.is(a, b);
-};
-
-console.log(strictEquals('toalla', 'toalla'));
+}
---
+++ imprt.js
@@ -0,0 +1,11 @@
+import { strictEquals } from './challenge.js';
+
+console.log(strictEquals(A, B));
+console.log(strictEquals(1, 1));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(1, '1'));
+console.log(strictEquals(true, false));
+console.log(strictEquals(false, true));
+console.log(strictEquals('water', 'oil'));
