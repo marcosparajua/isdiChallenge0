b1b584018c3a087baed008e275c3b617e2945781
marcos parajua <marcosparajua@gmail.com>
2024-02-15 09:13:16 +0100

    simplify code by joining two lines into one

--- challenge.js
+++ challenge.js
@@ -1,6 +1,11 @@
 export function strictEquals(a, b) {
   if (Number.isNaN(a) && Number.isNaN(b)) return false;
-  if (Object.is(a, 0) && Object.is(b, -0)) return true;
-  if (Object.is(a, -0) && Object.is(b, 0)) return true;
+  // if (Object.is(a, 0) && Object.is(b, -0)) return true;
+  // if (Object.is(a, -0) && Object.is(b, 0)) return true;
+  if (
+    (Object.is(a, 0) && Object.is(b, -0)) ||
+    (Object.is(a, -0) && Object.is(b, 0))
+  )
+    return true;
   return Object.is(a, b);
 }
